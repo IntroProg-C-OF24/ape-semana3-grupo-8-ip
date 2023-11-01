5f81919f6af83a44ca811598841d5e6299ff791f
PedroYep <psyepez2@utpl.edu.ec>
2023-10-31 20:26:32 -0500

    Ejercicio04

---
+++ Ejercicio04/src/Ejercicio04.java
@@ -0,0 +1,16 @@
+package Ejercicio04;
+
+public class Ejercicio04 {
+
+    public static void main(String[] args) {
+        //Solución a la problemática
+        int var1 = 20;
+        int var2 = 1;
+        int var3 = 3;
+        int var4 = 2;
+        int resultado = var1 / (var2 + var3) + var4;
+        boolean resultadofinal = resultado >= var3;
+        System.out.println("Resultado final es: "+resultadofinal);
+    }
+
+}
