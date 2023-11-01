80ec623aabd2a026cf0ca7df28b40ce9ecc7e1c8
PedroYep <psyepez2@utpl.edu.ec>
2023-10-31 22:14:46 -0500

    Ejercicios resueltos

---
+++ Ejercicio08/Ejercicio08/src/ejercicio08/Ejercicio08.java
@@ -0,0 +1,17 @@
+package ejercicio08;
+public class Ejercicio08 {
+    public static void main(String[] args) {
+        int var1 = 10;
+        int var2 = 9;
+        int var3 = 20;
+        int var4 = 500;
+        int var5 = 2;
+        int var6 = 3;
+        int var7 = 200;
+        int var8 = 50;
+        int var9 = 51;
+        boolean respuesta = ((var1 * var2) + var3 - var4/var5 + Math.pow(var6, 2)>= var7)||(var8 >= var8 - var8 + var9);
+        System.out.println("La respuesta es: " + respuesta);
+//Autor: Pedro
+    }
+}
---
+++ Ejercicio10/Ejercicio10/src/ejercicio10/Ejercicio10.java
@@ -0,0 +1,13 @@
+package ejercicio10;
+public class Ejercicio10 {
+    public static void main(String[] args) {
+        int var1=25;
+        int var2=10;
+        int var3=100;
+        int var4=5;
+        int var5=2;
+        boolean respuesta = (Math.sqrt(var1)*var2>=var3)&&(true)||(false)||(var2/var4>=var5);
+        System.out.println("La respuesta es: " + respuesta);
+        //Autor: Pedro
+    }
+}
